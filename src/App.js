940f493a485b6d6422ff2abab7c5a3543e2c135d
Ricky <rickdhanota@gmail.com>
2020-09-13 23:16:34 -0700

    fixed the div issue, debugging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="app">
       <h1>Hello World</h1>
       {/* Header, essentially the navbar */}
+      <Header />
       {/* Home, everything below the navbar */}
     </div>
   );
