de2d51de7d4495bbecb5482282a4aa879d3a3331
Ricky <rickdhanota@gmail.com>
2020-09-14 22:23:56 -0700

    adding a product component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Home from './Home'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* Header, essentially the navbar */}
       <Header />
       {/* Home, everything below the navbar */}
+      <Home />
     </div>
   );
 }
