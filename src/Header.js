bd7b06cedee648204e5e1bd574f898de384e39fa
Ricky <rickdhanota@gmail.com>
2020-09-14 20:20:10 -0700

    added some more css to the header

--- src/Header.js
+++ src/Header.js
@@ -14,22 +14,22 @@ function Header() {
 
             <div className='header__nav'>
 
-            <div className = 'header__option'>
-                <span className = 'header__optionLineOne'>Hello Guest</span>
-                <span className = 'header__optionLineTwo'>Sign In</span>
-            </div>
+                <div className = 'header__option'>
+                    <span className = 'header__optionLineOne'>Hello Guest</span>
+                    <span className = 'header__optionLineTwo'>Sign In</span>
+                </div>
 
 
-            <div className = 'header__option'>
-                <span className = 'header__optionLineOne'>Return</span>
-                <span className = 'header__optionLineTwo'>& Orders</span>
-            </div>
+                <div className = 'header__option'>
+                    <span className = 'header__optionLineOne'>Return</span>
+                    <span className = 'header__optionLineTwo'>& Orders</span>
+                </div>
 
 
-            <div className = 'header__option'>
-                <span className = 'header__optionLineOne'>Your</span>
-                <span className = 'header__optionLineTwo'>Prime</span>
-            </div>
+                <div className = 'header__option'>
+                    <span className = 'header__optionLineOne'>Your</span>
+                    <span className = 'header__optionLineTwo'>Prime</span>
+                </div>
 
             </div>
 
