d27b2ef4fe40aed12ffbb591defbf8d0cb946ba6
Ricky <rickdhanota@gmail.com>
2020-09-15 00:06:24 -0700

    connecting the components and passing information through props

--- src/Home.js
+++ src/Home.js
@@ -10,15 +10,19 @@ function Home() {
                 <img className = 'home__image' src ="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"></img>
 
                 <div className = "home__row">
-                    <Product />
+                    <Product title = "The lean start up" price = {29.99} image = "https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" rating = {5}></Product>
+                    
+                    <Product></Product>
                 </div>
 
                 <div className = "home__row">
-                    
+                    <Product></Product>
+                    <Product></Product>
+                    <Product></Product>
                 </div>
 
                 <div className = "home__row">
-                    
+                    <Product></Product>
                 </div>
             </div>
 
--- src/Product.js
+++ src/Product.js
@@ -2,23 +2,20 @@ import React from 'react';
 import './Product.css';
 
 
-function Product() {
+function Product({ title, image, price, rating }) {
     return (
         <div className = "product">
             <div className = 'product__info'>
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <p className = "product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className = "product__rating">
-                    <p> 🌟 </p>
-                    <p> 🌟 </p>
-                    <p> 🌟 </p>
+                    {Array(rating).fill().map((_,i) => (<p> 🌟 </p> ))}
                 </div>
             </div>
-
-            <img src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"></img>
+            <img src={image}></img>
             <button>Add to Basket</button>
         </div>
     )
