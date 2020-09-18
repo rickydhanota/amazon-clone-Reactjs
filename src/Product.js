724aca5007f1234d2a61c3f438c24ad7be9f7c22
Ricky <rickdhanota@gmail.com>
2020-09-17 23:38:16 -0700

    added more react context API stuff

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 
-function Product({ title, image, price, rating }) {
+function Product({id, title, image, price, rating }) {
+    const [{ basket }, dispath] = useStateValue();
+
+    console.log("this is the basket >>>>>>", basket);
+    const addToBasket = () => {
+        dispath({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+
+
     return (
         <div className = "product">
             <div className = 'product__info'>
@@ -16,7 +34,7 @@ function Product({ title, image, price, rating }) {
                 </div>
             </div>
             <img src={image}></img>
-            <button>Add to Basket</button>
+            <button onClick = {addToBasket}>Add to Basket</button>
         </div>
     )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,11 +3,16 @@ export const initialState = {
 };
 
 const reducer = (state, action) => {
+    console.log(action);
     switch (action.type){
         case "ADD_TO_BASKET":
             return {
                 ...state,
                 basket: [...state.basket, action.items],
             };
+        default:
+            return state;
     }
 };
+
+export default reducer;
