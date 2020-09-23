8f27e84652e271660e55bdfe34f9ab24c368daed
Ricky <rickdhanota@gmail.com>
2020-09-22 23:18:55 -0700

    Creating the login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
 import {BrowserRouter as Router, Switch, Route}
 from 'react-router-dom';
 
@@ -12,17 +13,21 @@ function App() {
     <Router>
       <div className="app">
 
-        <Header />
         {/* The main navbar, and because this will be the same navbar on every page we put this outside of the switch */}
 
           <Switch>
+            <Route path = "/login">
+              <Login />
+            </Route>
 
             <Route path = "/checkout">
+              <Header />
               <Checkout />
             </Route>
 
             <Route path = "/">
               {/* Make sure the home link '/' is the last one, and also this is where we direct to the different and various components/pages */}
+              <Header />
               <Home />
             </Route>
 
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,16 @@
-import React from 'react'   
+import React from 'react';
 import './CheckoutProduct.css';
+import { useStateValue } from './StateProvider';
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+    const[{ basket }, dispatch] = useStateValue();
+    const removeFromBasket = () => {
+        dispatch({
+            type: "REMOVE_FROM_BASKET",
+            id: id,
+        })
+    }
+ 
     return (
         <div className = "checkoutProduct">
             <img className = "checkoutProduct__image" src = {image} />
@@ -15,7 +24,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
                 <div className = "CheckoutProduct__rating">
                     {Array(rating).fill().map((_,i) => ( <p> 🌟 </p> ))}
                 </div>
-                <button>Remove from Basket</button>
+                <button onClick = {removeFromBasket}>Remove from Basket</button>
             </div>
 
         </div>
