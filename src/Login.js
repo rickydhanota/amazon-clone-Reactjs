51a5dc26a1180a1d8e482d7a0692492cd5add447
Ricky <rickdhanota@gmail.com>
2020-09-22 23:52:29 -0700

    adding more login styling

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
     return (
         <div className = "login">
-            
-            <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
+            <Link to = "/">
+                <img className = "login__logo" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
+            </Link>
 
+            <div className = "login__container">
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="text"></input>
+                    <h5>Password</h5>
+                    <input type="password"></input>
+
+                    <button className = "login__signInButton">Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to THE FAKE AMAZON CLONE MADE BY RICKY DHANOTA Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <button className = "login__registerButton">Create your FAKE AMAZON Account</button>
+            </div>
 
         </div>
     )
