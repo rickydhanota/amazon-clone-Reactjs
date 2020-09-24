8a9e4a19524374a6656170de9b2018995cf2e632
Ricky <rickdhanota@gmail.com>
2020-09-24 00:00:05 -0700

    more login

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBvAv237DAgMJQu38BZLU7m4VA8TLqvOkg",
     authDomain: "clone-5e38d.firebaseapp.com",
@@ -9,3 +11,5 @@ const firebaseConfig = {
     appId: "1:716277565799:web:dbe1ea42f056729f3947c2",
     measurementId: "G-F07TBC78TB"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
