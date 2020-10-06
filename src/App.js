9c5877744c9a0ba1805f509fb5afc469cb96b178
Ricky <rickdhanota@gmail.com>
2020-10-05 21:33:32 -0700

    adding payment functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
+import Payment from './Payment';
 import {BrowserRouter as Router, Switch, Route}
 from 'react-router-dom';
 import { auth } from './firebase';
@@ -55,7 +56,7 @@ function App() {
 
             <Route path = "/payment">
               <Header />
-              <Checkout />
+              <Payment />
             </Route>
 
             <Route path = "/">
