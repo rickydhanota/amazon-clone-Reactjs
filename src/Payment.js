ac97fd5e6c95ff2a5117025e9a92c6318dd9e264
Ricky <rickdhanota@gmail.com>
2020-10-05 21:40:04 -0700

    creating out the page

--- src/Payment.js
+++ src/Payment.js
@@ -4,7 +4,27 @@ import "./Payment.css"
 function Payment() {
     return (
         <div className = "payment">
-            
+            <div className="payment__container">
+
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Delivery Address</h3>
+                    </div>
+                </div>
+
+
+
+                <div className="payment__section">
+                    
+                </div>
+
+
+
+                <div className="payment__section">
+                    
+                </div>
+
+            </div>
         </div>
     )
 }
