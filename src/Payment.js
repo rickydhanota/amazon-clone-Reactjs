bc3811aca7fb7b49736ba8b31443151ad472cb8d
Ricky <rickdhanota@gmail.com>
2020-10-13 20:24:01 -0700

    added a button which is disabled

--- src/Payment.js
+++ src/Payment.js
@@ -12,9 +12,11 @@ function Payment() {
     const [{ basket, user }, dispatch] = useStateValue();
     const stripe = useStripe();
     const elements = useElements;
+    const [succeeded, setSucceeded] = useState(false)
+    const [processing, setProcessing] = useState("")
 
     const[error, setError] = useState(null)
-    const[disbaled, setDisabled] = useState(true)
+    const[disabled, setDisabled] = useState(true)
 
     const handleChange = event => {
         //Listen for card changes in the CardElement
@@ -86,6 +88,9 @@ function Payment() {
                                 thousandSeparator = {true}
                                 prefix = {"$"}
                                 /> 
+                                <button disabled = {processing || disabled || succeeded}>
+                                    <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                                </button>
                             </div>
                         </form>
                     </div>
